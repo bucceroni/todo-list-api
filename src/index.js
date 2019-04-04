c9ecfe9b614bea94592a1c62c6926f4a754b2285
bucceroni <bucceroni@hotmail.com>
2019-04-04 02:10:58 -0300

    fix deploy heroku

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/todos", todo);
-let port = 3000;
+let port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log("Server is up and running on port numner " + port);
