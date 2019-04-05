81fd67287ea0420a8e078ac711f7b02b012f84ac
bucceroni <bucceroni@hotmail.com>
2019-04-04 22:13:23 -0300

    add cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require('cors')
 
 //db
 require("./config");
@@ -11,8 +12,9 @@ const todo = require("./routes/todoRoute");
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors())
 app.use("/todos", todo);
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log("Server is up and running on port numner " + port);
